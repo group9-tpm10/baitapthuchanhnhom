ac9a21c54d086ea7badf2eb2c87cb655cd631ca1
Admin <Admin@172.25.209.232>
2024-03-12 18:00:06 +0700

    "Quân up code"

--- src/btth/btth.java
+++ src/btth/btth.java
@@ -5,6 +5,8 @@ public class btth {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Leader create project");
+		
+		System.out.println("Quân Hello World");
 	}
 
 }
