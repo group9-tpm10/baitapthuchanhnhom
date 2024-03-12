08ae82b2437645529ec6b99b8c833a930cfea699
trinh ne <121652389+Mewww2k4@users.noreply.github.com>
2024-03-12 19:10:30 +0700

    upcode lan 1

--- src/btth/btth.java
+++ src/btth/btth.java
@@ -5,6 +5,10 @@ public class btth {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Leader create project");
+		
+		System.out.println("Thuy trinh command");
+		
+		
 	}
 
 }
