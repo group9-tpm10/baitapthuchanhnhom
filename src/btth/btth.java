a847232e7ac82b271b5f6d0cd2a659af360a1efe
Nguyen Hoang Anh <hoanganhfreelancer@gmail.com>
2024-03-12 19:43:01 +0700

    add hoanglinh

--- src/btth/btth.java
+++ src/btth/btth.java
@@ -5,10 +5,9 @@ public class btth {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Leader create project");
-		
-		System.out.println("Thuy trinh commands");
-		
-		
+
+		System.out.println("NGuyễn Thái Hoàng Linh");
+
 	}
 
 }
